2a23198a47aebe20c8d20278e3f4c36617382cac
Michael de Groot <specamps@gmail.com>
2016-06-18 13:55:09 +0200

    log.js is alive

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,27 @@ const gulp  = require('gulp')
 const watch = require('gulp-watch')
 
 gulp.task('default', () => {
-  gulp.watch([
-    'lib/**/*',
-  ], {debounceDelay: 2000}, (() => {
-    console.log('Files changed, reinstalling globally')
-    reinstall()
-  }))
+  console.log('First time install')
+  reinstall(() => {
+    console.log('Install done!')
+    gulp.watch([
+      'lib/**/*',
+    ], {debounceDelay: 5000}, (() => {
+      console.log('Files changed, reinstalling globally')
+      reinstall(() => {
+        console.log('Install done!')
+      })
+    }))
+  })
 })
 
-const reinstall = () => {
+const reinstall = cb => {
   const exec = require('child_process').exec
   const child = exec('npm install -g', (error, stdout, stderr) => {
     if (error) {
       throw new Error(error)
     } else {
-      console.log('Done!')
+      cb()
     }
   })
 }
-
-reinstall()
