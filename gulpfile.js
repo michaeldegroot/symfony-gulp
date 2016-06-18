35604a31e783e4df8d8194c902c0d766e8e64bba
Michael de Groot <specamps@gmail.com>
2016-06-18 12:16:20 +0200

    Getting somewhere :)

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const gulp  = require('gulp')
+const watch = require('gulp-watch')
+
+gulp.task('default', () => {
+  gulp.watch([
+    'lib/**/*',
+  ], {debounceDelay: 2000}, (() => {
+    console.log('Files changed, reinstalling globally')
+    const exec = require('child_process').exec
+    const child = exec('npm install -g', (error, stdout, stderr) => {
+      if (error) {
+        throw new Error(error)
+      } else {
+        console.log('Done!')
+      }
+    })
+  }))
+})
